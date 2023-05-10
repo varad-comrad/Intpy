f981202d052a379f3b299fe16fa94d843558af79
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-05-10 14:39:39 -0300

    second iteration of benchmarking

--- fibonacci.py
+++ fibonacci.py
@@ -1,62 +1,95 @@
 from intpy.intpy import initialize_intpy, deterministic
 import time
 import matplotlib.pyplot as plt
+import sys
+import multiprocessing
+import numpy as np
+import pandas as pd
 
 
 @deterministic
 def fibonacci_recursive(n):
-    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2) if n > 2 else 1
+    if n < 2:
+        return n
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2) 
 
 @deterministic
 def fibonacci_iterative(n):
-    if n <=2:
-        return 1
-    a = 1
-    b = 1
-    for i in range(n-1):
-        c = a+b
-        a = b
-        b = c
-    return b
-
-def fibonacci_recursive_no_intpy(n):
-    return fibonacci_recursive_no_intpy(n-1) + fibonacci_recursive_no_intpy(n-2) if n > 2 else 1
-
-
-def fibonacci_iterative_no_intpy(n):
-    pass
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+    return a
 
 class FibonacciBenchmark:
-    def __init__(self, fun1: function, fun2: function):
+    def __init__(self, fun1, fun2, title = ''):
         self.fun1 = fun1
         self.fun2 = fun2
-        self.results_fun1: list = []
-        self.results_fun2: list = []
-        self.cnt_fun1: int = 0
-        self.cnt_fun2: int = 0
+        self.results_fun1 = []
+        self.results_fun2 = []
+        self.cnt_fun1 = 0
+        self.cnt_fun2 = 0
+        self.title = title
 
-    
+
     def benchmark_fun1(self, *args):
         self.results_fun1.append([])
         for i in range(args[0]):
+            t = time.perf_counter()
+            self.fun1(i)
             self.results_fun1[self.cnt_fun1].append(
-                time.perf_counter(self.fun1(i)))
-        self.cnt_fun1+=1
-            
-    
+                time.perf_counter() - t)
+        self.cnt_fun1 += 1
+
+
     def benchmark_fun2(self, *args):
         self.results_fun2.append([])
         for i in range(args[0]):
+            t = time.perf_counter()
+            self.fun2(i)
             self.results_fun2[self.cnt_fun2].append(
-                time.perf_counter(self.fun2(i)))
+                time.perf_counter() - t)
         self.cnt_fun2 += 1
+    
+    
+    def to_numpy(self):
+        aux_results_fun1 = np.array(self.results_fun1)
+        aux_results_fun2 = np.array(self.results_fun2)
+        return aux_results_fun1.mean(axis=0), aux_results_fun2.mean(axis=0)
 
+
+def plot_graphic(bench1, bench2, n: int):
+    aux1, subtitles = (bench1.to_numpy(), bench2.to_numpy()), ['Recursive Fibonacci', 'Iterative Fibonacci']
+    fig, axes = plt.subplots(2, 2, figsize=(16, 10))
+    fig.suptitle('benchmark')
+    for i in range(2): 
+        for j in range(2):
+            axes[i,j].plot(np.arange(n), aux1[i][j], color='bisque')
+            axes[i,j].set_title(subtitles[j])
+            axes[i,j].grid(color='lawngreen', linestyle='--')
+            axes[i,j].set_facecolor('royalblue')
+    fig.show()
+
+def improvement(bench1, bench2):
+    aux1, aux2 = bench1.to_numpy(), bench2.to_numpy()
+    vec = (aux1[0]-aux2[0])/aux2[0]
+    return dict(enumerate(vec))
 
 @initialize_intpy(__file__)
-def main():
-    print(fibonacci_iterative(3))
-    # benchmark = FibonacciBenchmark(fibonacci_recursive, fibonacci_iterative)
-    # benchmark_no_intpy = FibonacciBenchmark(fibonacci_recursive_no_intpy, fibonacci_iterative_no_intpy)
-
+def main(n):
+    benchmark = FibonacciBenchmark(deterministic(fibonacci_recursive), deterministic(fibonacci_iterative), 'Intpy')
+    benchmark_no_intpy = FibonacciBenchmark(fibonacci_recursive, fibonacci_iterative, 'No Intpy')
+    for i in range(n): 
+        benchmark.benchmark_fun1(n)
+        benchmark.benchmark_fun2(n)
+        benchmark_no_intpy.benchmark_fun1(n)
+        benchmark_no_intpy.benchmark_fun2(n)
+    plot_graphic(benchmark, benchmark_no_intpy, n)
+    d = improvement(benchmark, benchmark_no_intpy)
+    print(pd.Series(d))
+    
+    input()
+    
 if __name__ == '__main__':
-    main()
+    n = int(sys.argv[1])
+    main(n)
