12327c7217092cfbf8a78e174fc95b433f0caea3
Fabricio Asfora <fabricio_asfora_002@hotmail.com>
2023-07-06 19:07:55 -0300

    Update fibnumbaspeed.py

--- fibnumbaspeed.py
+++ fibnumbaspeed.py
@@ -1,11 +1,12 @@
 import numba as nb, time, sys
-
+from speedupy import initialize_intpy, deterministic
 
 @deterministic
 @nb.njit
 def fib(n):
     return fib(n-1) + fib(n-2) if n>2 else n
 
+@initialize_intpy(__file__)
 def main(n):
     t=time.perf_counter()
     fib(n)
